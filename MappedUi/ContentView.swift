b00e622eaa6eef482ebe1729dcd9c7005c67fa53
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-03-31 21:13:36 -0300

    Initial Commit

---
+++ MappedUi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MappedUi
+//
+//  Created by Sebastian Marquez Rojas on 31/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MappedUi/MappedUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MappedUiApp.swift
+//  MappedUi
+//
+//  Created by Sebastian Marquez Rojas on 31/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MappedUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
