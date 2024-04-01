a444a3bf02a355b2860e0570e2b3d8923c83e1a3
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-03-31 22:48:32 -0300

    map created.

--- MappedUi/ContentView.swift
+++ MappedUi/ContentView.swift
@@ -6,16 +6,24 @@
 //
 
 import SwiftUI
-
+import MapKit
 struct ContentView: View {
+    //@State private var newMapCamera:MapCameraPosition = .automatic //initial position
+    @State private var newMapCamera:MapCameraPosition = .region(.userRegion)
     var body: some View {
-        VStack {
-            Image(systemName: "car.fill")
-                .imageScale(.large)
-                .foregroundStyle(.red)
-            Text("Hello, world!")
-        }
-        .padding()
+        Map(position:$newMapCamera)
+    }
+}
+
+extension CLLocationCoordinate2D{
+    static var userLocation:CLLocationCoordinate2D{
+        return .init(latitude: 25.7602 , longitude: -80.2369)
+    }
+}
+
+extension MKCoordinateRegion{
+    static var userRegion:MKCoordinateRegion{
+        return .init(center:.userLocation,latitudinalMeters: 10000,longitudinalMeters: 10000)
     }
 }
 
