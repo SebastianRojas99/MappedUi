4b8b7ba163966782b85c1d71fb0360dee98f0c87
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-04-04 23:05:47 -0300

    look around view added.

--- MappedUi/LocationView.swift
+++ MappedUi/LocationView.swift
@@ -12,6 +12,7 @@ import MapKit
 struct LocationView: View {
     @Binding var markerSelector : MKMapItem?
     @Binding var showLocation : Bool
+    @State private var lookAround:MKLookAroundScene?
     var body: some View {
         VStack{
             Text(markerSelector?.placemark.name ?? "")
@@ -24,12 +25,31 @@ struct LocationView: View {
                 .foregroundStyle(.gray)
                 .lineLimit(2)
                 .padding(.trailing)
-            Text(markerSelector?.placemark.countryCode ?? "" )
-                  .font(.footnote)
-                .bold()
-                .foregroundStyle(.gray)
-                .lineLimit(2)
+            if let scene = lookAround{
+                LookAroundPreview(initialScene: scene)
+                    .frame(height: 200)
+                    .cornerRadius(12)
+                    .padding(.all)
+            }
         }.padding(.all)
+            .onAppear{
+                fetchAroundPreview()
+            }
+            .onChange(of: markerSelector){ oldValue,newValue in
+                fetchAroundPreview()
+            }
+                
     }
 }
 
+extension LocationView{
+    func fetchAroundPreview(){
+        if let markerSelector{
+            lookAround = nil
+            Task{
+                let request = MKLookAroundSceneRequest(mapItem: markerSelector)
+                lookAround = try? await request.scene
+            }
+        }
+    }
+}
